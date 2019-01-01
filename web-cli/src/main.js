596cd311936707bc7fa6d30cbc1dbe251736a760
an55555 <156081289@qq.com>
2019-01-01 23:27:53 +0800

    ``

--- web-cli/src/main.js
+++ web-cli/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import toast from './components/toast/index'
+
+Vue.use(toast)
 
 /* eslint-disable no-new */
 new Vue({
