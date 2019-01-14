d66193595fedc2cdaf0edf3100e63ba291851a1a
lanjz <lanjz@mingyuanyun.com>
2019-01-14 14:29:11 +0800

    添加schema

--- web-cli/src/pages/schema.vue
+++ web-cli/src/pages/schema.vue
@@ -32,47 +32,86 @@
               </select>
             </div>
           </div>
-          <div class="form-group flex" v-if="schema.type === 'radio' || 'select'">
+          <div class="form-group flex" v-if="schema.type === 'radio' || schema.type === 'select'">
             <div class="form-label-layout">
               选项：
             </div>
-            <div class="flex flex-1 align-items-center form-content wrap">
-              <div class="add-options-item"
-                   v-for="(item, index) in schema.options"
-                   contenteditable="true">
-                {{item}}
+            <div class="flex flex-1 form-content wrap direction-column">
+              <div class="flex align-items-center wrap">
+                <div class="add-options-item"
+                     v-for="(item, index) in schema.options"
+                     contenteditable="true">
+                  {{item}}
+                </div>
               </div>
-              <input type="text" class="form-input add-options-input" v-focus:select>
-              <div class="add-options-item" @click="doAddSchemaOption">add</div>
+              <div flex align-items-center wrap>
+                <input type="text" class="form-input add-options-input" v-focus:select v-model="newOptionValue">
+                <div
+                  class="add-options-item add-options-btn"
+                  :class="{'disable-btn': !newOptionValue}"
+                  @click.trim="doAddSchemaOption">
+                  add
+                </div>
+              </div>
             </div>
           </div>
-          <div class="form-group flex" v-if="schema.type === 'input' || 'textarea'">
+          <!--单选默认值-->
+          <div class="form-group flex" v-if="schema.type === 'input'">
             <div class="form-label-layout">
               默认值：
             </div>
             <div class="flex flex-1 align-items-center">
-              <input type="text" class="form-input">
+              <input type="text" class="form-input" v-model="schema.default">
             </div>
           </div>
+          <!--多行默认值-->
+          <div class="form-group flex" v-if="schema.type === 'textarea'">
+            <div class="form-label-layout">
+              默认值：
+            </div>
+            <div class="flex flex-1 align-items-center">
+              <textarea type="text" class="form-input" v-model="schema.default" />
+            </div>
+          </div>
+          <!--radios默认值-->
           <div class="form-group flex" v-if="schema.type === 'radio'&&schema.options.length">
             <div class="form-label-layout">
               默认值：
             </div>
             <div class="flex flex-1 align-items-center">
-              <div class="add-options-item" v-for="(item, index) in schema.options">
-                <input type="radio" v-model="schema.default">{{item.name}}
+              <div
+                class="add-options-item radio-style"
+                :class="{'act':!schema.default}"
+                @click="schema.default = ''"
+              >
+                无
+              </div>
+              <div
+                class="add-options-item radio-style"
+                v-for="(item, index) in schema.options"
+                :class="{'act':item.id === schema.default}"
+              >
+                <input type="radio" class="form-radio" :value="item.id" :key="item.id" v-model="schema.default">{{item.name}}
               </div>
             </div>
           </div>
-          <div class="form-group flex" v-if="schema.type === 'radio'&&schema.options.length">
+          <!--select默认值-->
+          <div class="form-group flex" v-if="schema.type === 'select'&&schema.options.length">
             <div class="form-label-layout">
               默认值：
             </div>
             <div class="flex flex-1 align-items-center">
-              <div class="add-options-item" v-for="(item, index) in schema.options">
-                <input type="checkBox" v-model="schema.arrDefault">{{item.name}}
+              <div
+                class="add-options-item radio-style"
+                v-for="(item, index) in schema.options"
+                :class="{'act':schema.arrDefault.indexOf(item.id) > -1}"
+              >
+                <input type="checkbox" class="form-radio" :value="item.id" :key="item.id" v-model="schema.arrDefault">{{item.name}}
               </div>
             </div>
+          </div>
+          <div class="form-group">
+            <div class="btn" @click="todoSaveSchema">提交</div>
           </div>
         </div>
       </div>
@@ -91,17 +130,15 @@
           arrDefault: [],
         },
         optionsIdAsc: 0,
-        temOptions: {
-          id: 1,
-          name: ''
-        },
         typeList: [
-          {alias: '单行文本', name: 'input'},
-          {alias: '多行文本', name: 'textarea'},
-          {alias: '单选', name: 'radio'},
-          {alias: '多选', name: 'select'},
-          {alias: '日期', name: 'time'},
-        ]
+          {alias: '单行文本', name: 'input', type: 'String'},
+          {alias: '多行文本', name: 'textarea', type: 'String'},
+          {alias: '单选', name: 'radio', type: 'String'},
+          {alias: '多选', name: 'select', type: 'Array'},
+          {alias: '日期', name: 'time', type: 'String'},
+          {alias: '标签', name: 'label', type: 'Array'},
+        ],
+        newOptionValue: ''
       }
     },
     methods: {
@@ -110,19 +147,53 @@
         this.schema.arrDefault = []
       },
       doAddSchemaOption() {
-        this.temOptions.id = this.optionsIdAsc + 1
-        let nameIndex = this.temOptions.id
-        let tempName = `未命名选项${nameIndex}`
-        while (this.schema.options.indexOf(tempName) > -1){
-          nameIndex += 1
-          tempName = `未命名选项${nameIndex}`
+        if(!this.newOptionValue) return
+        if (this.schema.options.findIndex(item => item.name === this.newOptionValue) > -1){
+          window.alert(`${this.newOptionValue}已存在`)
+          return
         }
-        while (this.schema.options.findIndex(item => item.name === tempName) > -1){
-          nameIndex += 1
-          tempName = `未命名选项${nameIndex}`
+        let temIndex = this.optionsIdAsc + 1
+        let temId = `option_${temIndex}`
+        while (this.schema.options.findIndex(item => item.id === temId) > -1){
+          temIndex += 1
+          temId = `option_${temIndex}`
         }
-        this.temOptions.name = tempName
-        this.schema.options.push('选项')
+        this.schema.options.push({
+          name: this.newOptionValue,
+          id: temId
+        })
+        this.newOptionValue = ''
+      },
+      todoSaveSchema() {
+        if(!this.schema.name) {
+          window.alert('请输入别名')
+          return
+        }
+        let validErr = null
+        const validType = Object.prototype.toString.call(this.schema.default)
+        switch (this.schema.type) {
+          case 'input':
+          case 'textarea':
+          case 'time':
+          case 'radio':
+            if(this.schema.default && validType !== '[object String]') {
+              validErr = new TypeError('类型值不是String类型')
+            }
+            break
+          case 'select':
+            if(this.schema.default && validType !== '[object Array]') {
+              validErr = new TypeError('类型值不是Array类型')
+            }
+            break
+        }
+        if(validErr) {
+          window.alert(validErr.message)
+          return
+        }
+        this.doSaveSchema()
+      },
+      async doSaveSchema() {
+        console.log('this.schema', this.schema)
       }
     }
   }
@@ -151,9 +222,14 @@
     margin-right: 7px;
     margin-bottom: 7px;
   }
+  .add-options-btn{
+    margin: 0;
+    transition: .4s;
+  }
 
   .add-options-input {
-    width: 100%;
+    width: 140px;
+    margin-right: 10px;
   }
   .form-content{
     max-width: 500px;
