61d7ced1c8214c7a8e45a116f5566c8baa6f2da0
lanjz <lanjz@mingyuanyun.com>
2019-01-30 11:21:23 +0800

    F: add loading

--- web-cli/src/main.js
+++ web-cli/src/main.js
@@ -4,12 +4,15 @@ import router from './router'
 import store from './store'
 import directive from './global/directive'
 import helloAlert from './components/messageBox/index'
+import helloLoading from './components/loading/index'
 
 
 Vue.config.devtools = true
 Vue.use(directive)
 
 Vue.prototype.$alert = helloAlert
+Vue.prototype.$showLoading = helloLoading.show
+Vue.prototype.$hideLoading = helloLoading.hide
 
 /* eslint-disable no-new */
 new Vue({
